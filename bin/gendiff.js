0676a25097749d55f0fcd01f4775b1355f7202bd
unInsomnia <101837517+uninsomnia@users.noreply.github.com>
2022-09-17 20:29:06 +0300

    set gendiff logic

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,29 +12,41 @@ program
     .action((filepath1, filepath2) => {
         const path1 = path.resolve(process.cwd(), filepath1);
         const path2 = path.resolve(process.cwd(), filepath2);
-        const data1 = fs.readFileSync(path1, 'utf-8');
-        const data2 = fs.readFileSync(path2, 'utf-8');
-        const data1Keys = data1.split('\n');
-        const data2Keys = data2.split('\n');
-        // let resultData1 = '';
-        // let resultData2 = '';
-        // let mapTarget = (data2Keys.length < data1Keys.length) ? data1Keys : data2Keys;
-        // mapTarget.map((key) => {
-        //     if (data1[key] === data2[key]) {
-        //         resultNeutral += `  ${key}: ${data1[key]},\n`;
-        //     } else {
-        //         if (data1[key] === undefined) {
-        //             resultData2 += `+ ${key}: ${data2[key]},\n`;
-        //         } else if (data2[key] === undefined) {
-        //             resultData1 += `- ${key}: ${data1[key]},\n`;
-        //         } else {
-        //             resultData1 += `- ${key}: ${data1[key]},\n`;
-        //             resultData2 += `+ ${key}: ${data2[key]},\n`;
-        //         }
-        //     }
-        // });
-        // const result = `{\n${resultData1}${resultNeutral}${resultData2}}`;
-            console.log(data1Keys, data2Keys);
+        const data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
+        const data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
+        const data1Keys = Object.getOwnPropertyNames(data1).sort();
+        const data2Keys = Object.getOwnPropertyNames(data2).sort();
+        const result = {
+                data1Changes: {},
+                data2Changes: {}
+        };
+        data1Keys.map((keyName) => {
+                if (_.has(data2, keyName)) {
+                        if (data1[keyName] === data2[keyName]) {
+                                result.data1Changes[`  ${keyName}`] = data1[keyName];
+                        } else {
+                                result.data1Changes[`- ${keyName}`] = data1[keyName];
+                        }
+                } else {
+                        result.data1Changes[`- ${keyName}`] = data1[keyName];
+                }
+        });
+        data2Keys.map((keyName) => {
+                if (_.has(data1, keyName)) {
+                        if (data1[keyName] !== data2[keyName]) {
+                                result.data2Changes[`+ ${keyName}`] = data2[keyName];
+                        }
+                } else {
+                        result.data2Changes[`+ ${keyName}`] = data2[keyName];
+                }
+        });
+        let resultStr = '{\n';
+        const data1Results = Object.entries(result.data1Changes);
+        const data2Results = Object.entries(result.data2Changes);
+        data1Results.forEach(([key, value]) => resultStr += `${key}: ${value}\n`);
+        data2Results.forEach(([key, value]) => resultStr += `${key}: ${value}\n`);
+        resultStr += '}'
+        console.log(resultStr);
     })
     .option('-f, --format <type>', 'output format')
     .parse();
